82a302a76d79888e694ddda08fdc127d7a115164
Niels van Rongen <vanrongen@gmail.com>
2016-09-11 15:06:16 +0200

    +: First commit

---
+++ build.js
@@ -0,0 +1,51 @@
+const glob = require('glob')
+const Metalsmith = require('metalsmith')
+const layouts = require('metalsmith-layouts')
+const assets = require('metalsmith-assets')
+const sass = require('metalsmith-sass')
+const markdown = require('metalsmith-markdown')
+const dataMarkdown = require('metalsmith-data-markdown')
+const contentful = require('contentful-metalsmith')
+
+const handlebars = require('handlebars')
+
+// add custom helpers to handlebars
+// https://github.com/superwolff/metalsmith-layouts/issues/63
+//
+// using the global handlebars instance
+glob.sync('helpers/*.js').forEach((fileName) => {
+  const helper = fileName.split('/').pop().replace('.js', '')
+
+  handlebars.registerHelper(
+    helper,
+    require(`./${fileName}`)
+  )
+})
+
+Metalsmith(__dirname)
+  .source('src')
+  .destination('build')
+  .use(contentful({
+    space_id: 'or4dsolesnus',
+    access_token: '7af4ab1d081fc76b9e0ddd7df3309c3d1cb9979e7a00ececdebdf1ab5334189e'
+  }))
+  .use(layouts({
+    engine: 'handlebars',
+    partials: 'partials'
+  }))
+  .use(assets({
+    source: 'assets/',
+    destination: 'assets/'
+  }))
+  .use(sass({
+    outputStyle: 'compressed'
+  }))
+  .use(markdown())
+  .use(dataMarkdown({
+    removeAttributeAfterwards: true
+  }))
+  .build(function (err) {
+    if (err) throw err
+
+    console.log('Successfully build metalsmith')
+  })
